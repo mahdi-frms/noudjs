808374d7897f202046f0d6d4a5eb729e1e7ef357
mahdi-frms <mahdif1380@outlook.com>
2021-10-12 23:06:50 +0330

    add socket, fs and timer

--- src/main.c
+++ src/main.c
@@ -5,7 +5,10 @@
 #include <unistd.h>
 #include <sys/file.h>
 #include <cwalk.h>
+#include <evloop.h>
 
+duk_context *glb_ctx;
+evloop_t glb_evloop;
 char *glb_path;
 
 char *load_file(const char *address, size_t *size)
@@ -49,10 +52,29 @@ duk_ret_t gprint(duk_context *ctx)
     return 0;
 }
 
+duk_ret_t gexit(duk_context *ctx)
+{
+    evloop_terminate(&glb_evloop);
+    return 0;
+}
+
 duk_ret_t load_module(duk_context *ctx)
 {
     char path[1024];
-    const char *rel_path = duk_to_string(ctx, 0);
+    const char *id = duk_to_string(ctx, 0);
+    if (strcmp(id, "fs") == 0)
+    {
+        evl_lib_fs(ctx);
+        duk_put_prop_string(ctx, 3, "exports");
+        return 0;
+    }
+    if (strcmp(id, "socket") == 0)
+    {
+        evl_lib_socket(ctx);
+        duk_put_prop_string(ctx, 3, "exports");
+        return 0;
+    }
+    const char *rel_path = id;
     cwk_path_join(glb_path, rel_path, path, 1024);
     char *src = load_file(path, NULL);
     duk_push_string(ctx, src);
@@ -69,28 +91,43 @@ void fatal_handler(void *_, const char *msg)
 
 int main(int argc, char *argv[])
 {
+    glb_ctx = duk_create_heap(NULL, NULL, NULL, NULL, fatal_handler);
+    evloop_initialize(&glb_evloop, 8);
+    duk_module_duktape_init(glb_ctx);
+
+    duk_get_global_string(glb_ctx, "Duktape");
+    duk_push_c_function(glb_ctx, load_module, 4);
+    duk_put_prop_string(glb_ctx, -2, "modSearch");
+    duk_pop(glb_ctx);
+
+    duk_push_c_function(glb_ctx, gprint, 1);
+    duk_put_global_string(glb_ctx, "print");
+    duk_push_c_function(glb_ctx, gexit, 0);
+    duk_put_global_string(glb_ctx, "exit");
+
+    duk_push_object(glb_ctx);
+    duk_put_global_string(glb_ctx, "Evloop");
+
+    evl_add_functions(glb_ctx);
+
     if (argc == 1)
     {
         return 1;
     }
     glb_path = argv[1];
 
-    duk_context *ctx = duk_create_heap(NULL, NULL, NULL, NULL, fatal_handler);
-    duk_module_duktape_init(ctx);
     char *script = load_file(glb_path, NULL);
 
     size_t len;
     cwk_path_get_dirname(glb_path, &len);
     glb_path[len] = '\0';
 
-    duk_get_global_string(ctx, "Duktape");
-    duk_push_c_function(ctx, load_module, 4);
-    duk_put_prop_string(ctx, -2, "modSearch");
-    duk_pop(ctx);
+    duk_eval_string_noresult(glb_ctx, script);
+    evloop_main_loop(&glb_evloop);
 
-    duk_push_c_function(ctx, gprint, 1);
-    duk_put_global_string(ctx, "print");
-    duk_eval_string_noresult(ctx, script);
-    duk_destroy_heap(ctx);
+    // finalization
+    free(script);
+    duk_destroy_heap(glb_ctx);
+    evloop_destroy(&glb_evloop);
     return 0;
 }
